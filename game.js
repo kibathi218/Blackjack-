d5284e42e0849c744d8eefa000f8f55586fbe8ac
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-09 11:18:35 -0500

    called variables from html

---
+++ game.js
@@ -0,0 +1,13 @@
+const dhan = document.querySelector(".dealers-hand");
+const dcard = document.querySelector(".dealers-cards");
+const phan = document.querySelector(".playerss-hand");
+const pcard = document.querySelector(".dealers-cards");
+
+const blackj = {
+    dhand,
+    dcards,
+    phands,
+    pcards,
+    deck: [],
+
+}
