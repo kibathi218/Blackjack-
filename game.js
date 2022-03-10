e1f8c98cd3c0f4f0bd5bc3d29c6070a58f4e6b65
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-10 11:01:39 -0500

    hit button draws card

--- game.js
+++ game.js
@@ -1,32 +1,31 @@
 blackjack = {
     dhan: document.querySelector(".dealers-hand"),
     dcard: document.querySelector(".dealers-cards"),
-    phan: document.querySelector(".playerss-hand"),
+    phan: document.querySelector(".players-cards"),
     pcard: document.querySelector(".dealers-cards"),
     playBtn: document.querySelector(".play-btn"),
 
     standBtn: document.getElementById("stand-btn"),
-
-    deck: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-
-    deckValues: {
-        A: [1, 11],
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 6,
-        7: 7,
-        8: 8,
-        9: 9,
-        10: 10,
-        J: 10,
-        Q: 10,
-        K: 10
-    },
-
     stood: false
 
+}
+
+let deck = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+const deckValues = {
+    A: [1, 11],
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 10,
+    Q: 10,
+    K: 10
 }
 
 const hitBtn = document.getElementById("hit-btn");
@@ -39,14 +38,15 @@ function test() {
 
 
 
-function randomcard() {
+function randomCard() {
     let idx = Math.floor(Math.random() * 12);
-    return blackjack[deck][idx];
+    return deck[idx];
 }
 
-function displayCard() {
+function displayCard(card) {
     let cardImg = document.createElement('img');
     cardImg.src = `/images/${card}.png`;
+    blackjack.phan.append(cardImg)
 
 }
 
@@ -59,7 +59,4 @@ function bjHit() {
 
 
 
-
-
 hitBtn.addEventListener("click", bjHit);
-standBtn.addEventListener("click", test);
