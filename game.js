d762d38b233cea27ec75c5ffb0dfd37e6f527729
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-10 08:22:51 -0500

    added card images

--- game.js
+++ game.js
@@ -1,20 +1,35 @@
-const dhan = document.querySelector(".dealers-hand");
-const dcard = document.querySelector(".dealers-cards");
-const phan = document.querySelector(".playerss-hand");
-const pcard = document.querySelector(".dealers-cards");
-const playBtn = document.querySelector(".play-btn");
-const hitBtn = document.getElementById("hit-btn");
-const standBtn = document.getElementById("stand-btn");
+blackjack = {
+    dhan: document.querySelector(".dealers-hand"),
+    dcard: document.querySelector(".dealers-cards"),
+    phan: document.querySelector(".playerss-hand"),
+    pcard: document.querySelector(".dealers-cards"),
+    playBtn: document.querySelector(".play-btn"),
+    hitBtn: document.getElementById("hit-btn"),
+    standBtn: document.getElementById("stand-btn"),
 
-// const blackj = {
-//     dhand,
-//     dcards,
-//     phands,
-//     pcards,
-//     deck: [],
+    deck: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+
+    deckValues: {
+        A: [1, 11],
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        J: 10,
+        Q: 10,
+        K: 10
+    },
+
+    stood: false
+
+}
 
 
-// }
 
 function test() {
     console.log("testnjr med");
@@ -22,3 +37,21 @@ function test() {
 
 hitBtn.addEventListener("click", test);
 standBtn.addEventListener("click", test);
+
+function bjHit() {
+    if (blackjack[stood] === false) {
+        let card = randomCard();
+        displayCard(card);
+    }
+}
+
+function randomcard() {
+    let idx = Math.floor(Math.random() * 12);
+    return blackjack[deck][idx];
+}
+
+function displayCard() {
+    let cardImg = document.createElement('img');
+    cardImg.src = `${card}.png`;
+
+}
