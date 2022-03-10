ba3b8808bed373a2995304101fc94e6cbabdd67c
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-10 07:08:11 -0500

    finished styling homepage

--- game.js
+++ game.js
@@ -2,14 +2,23 @@ const dhan = document.querySelector(".dealers-hand");
 const dcard = document.querySelector(".dealers-cards");
 const phan = document.querySelector(".playerss-hand");
 const pcard = document.querySelector(".dealers-cards");
+const playBtn = document.querySelector(".play-btn");
+const hitBtn = document.getElementById("hit-btn");
+const standBtn = document.getElementById("stand-btn");
 
-const blackj = {
-    dhand,
-    dcards,
-    phands,
-    pcards,
-    deck: [],
-    draw,
+// const blackj = {
+//     dhand,
+//     dcards,
+//     phands,
+//     pcards,
+//     deck: [],
 
 
+// }
+
+function test() {
+    console.log("testnjr med");
 }
+
+hitBtn.addEventListener("click", test);
+standBtn.addEventListener("click", test);
